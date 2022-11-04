a9ada707ee0bd666a037648d7b8a260edab42879
czycs <95682736+czycs@users.noreply.github.com>
2022-11-04 11:07:28 +0100

    price to numeric fix

--- 2022_10_30_read_data.R
+++ 2022_10_30_read_data.R
@@ -17,11 +17,15 @@ berlin_calendar <-  read_csv("Berlin/2022_09_15_Berlin_calendar.csv.gz")
 berlin_listings <- read_csv("Berlin/2022_09_15_Berlin_listings.csv",
                             col_types = cols(id = col_character(),
                                              host_id = col_character()))
+berlin_listings$price<- gsub(",","", berlin_listings$price)
+berlin_listings$price<- as.numeric(gsub("\\$","", berlin_listings$price))
 
 # full listing db
 berlin_listings_full <- read_csv("Berlin/2022_09_15_Berlin_listings.csv.gz",
                                  col_types = cols(id = col_character(),
                                                   scrape_id = col_character()))
+berlin_listings_full$price<- gsub(",","", berlin_listings_full$price)
+berlin_listings_full$price<- as.numeric( gsub("\\$","", berlin_listings_full$price))
 # review date listing_id
 berlin_reviews_date <- read_csv("Berlin/2022_09_15_Berlin_reviews.csv",
                                 col_types = cols(listing_id = col_character(),
@@ -41,11 +45,16 @@ munich_calendar <-  read_csv("Munich/2022_09_21_Munich_calendar.csv.gz")
 munich_listings <- read_csv("Munich/2022_09_21_Munich_listings.csv",
                             col_types = cols(id = col_character(),
                                              host_id = col_character()))
+munich_listings$price<- gsub(",","", munich_listings$price)
+munich_listings$price<- as.numeric(gsub("\\$","", munich_listings$price))
 
 # full listing db
 munich_listings_full <- read_csv("Munich/2022_09_21_Munich_listings.csv.gz",
                                  col_types = cols(id = col_character(),
                                                   scrape_id = col_character()))
+munich_listings_full$price<- gsub(",","", munich_listings_full$price)
+munich_listings_full$price<- as.numeric( gsub("\\$","", munich_listings_full$price))
+
 # review date listing_id
 munich_reviews_date <- read_csv("Munich/2022_09_21_Munich_reviews.csv",
                                 col_types = cols(listing_id = col_character(),
